e158bf9006df5c92d2a135d979fc5962d06727f2
Paige Williams <paw145@humboldt.edu>
2019-01-25 11:45:50 -0800

    Add styling to category component.

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -5,11 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent implements OnInit {
+export class CategoryComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
