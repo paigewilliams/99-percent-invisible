a701fe1e7a04bdefca788e3f9690f90fa6e77ea7
Paige Williams <paw145@humboldt.edu>
2019-01-25 11:27:50 -0800

    Create body component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FeaturedComponent } from './featured/featured.component';
+import { BodyComponent } from './body/body.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FeaturedComponent
+    FeaturedComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
