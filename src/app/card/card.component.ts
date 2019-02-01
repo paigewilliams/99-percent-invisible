ff00ac1fea3596c5fffa09b8f99e47e8eb883ed2
Paige Williams <paw145@humboldt.edu>
2019-02-01 15:06:24 -0800

    Add input to child card component.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Episode } from '../models/episode.model';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-
+  @Input() selectedEpisode
 
 }
