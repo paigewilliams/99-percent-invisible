a9c18567719d33875eef82399cdbf98a8d2b3a33
Paige Williams <paw145@humboldt.edu>
2019-02-01 13:52:25 -0800

    Create Episode object for each episode in response and push into master array.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PodcastApiService } from './podcast-api.service';
+import { Episode } from './models/episode.model';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import { PodcastApiService } from './podcast-api.service';
 })
 export class AppComponent implements OnInit {
   title = '99 Percent Invisible';
+  masterEpisodes : Episode[] = [];
 
   episodes: Object = null;
   constructor( private podcastService: PodcastApiService ){}
@@ -20,7 +22,18 @@ export class AppComponent implements OnInit {
   getPodcasts(){
     this.podcastService.getEpisodes().subscribe(response => {
       this.episodes = response
-      console.log(this.episodes)
+      this.createMasterEpisodes(this.episodes)
     })
+  }
+
+  createMasterEpisodes(episodes: any){
+    episodes.results.forEach(episode => {
+      const audio = episode.audio;
+      const description = episode.description_original;
+      const publishDate = episode.pub_date_ms;
+      const title = episode.title_original;
+      this.masterEpisodes.push(new Episode(audio, description, publishDate, title))
+    })
+    console.log(this.masterEpisodes);
   }
 }
